9b2cdb0a9f808b83f3f8872327c7e7fbacc2fe61
tcondeixa <tscondeixa@gmail.com>
2023-11-30 23:29:11 +0100

    adding tscondeixa keymaps

---
+++ keyboards/crkbd/keymaps/tscondeixa/keycodes.h
@@ -0,0 +1,8 @@
+#pragma once
+
+enum layers {
+    LAYER_LETTERS,
+    LAYER_NUMBERS,
+    LAYER_SYMBOLS,
+    LAYER_CONTROLS,
+};
---
+++ keyboards/crkbd/keymaps/tscondeixa/oled.c
@@ -0,0 +1,62 @@
+#include QMK_KEYBOARD_H
+#include "oled.h"
+#include "keycodes.h"
+
+oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+    if (!is_keyboard_master()) {
+        return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
+    }
+    return rotation;
+}
+
+void oled_render_layer_state(void) {
+    oled_write_P(PSTR("Layer: "), false);
+    switch (get_highest_layer(layer_state)) {
+        case LAYER_LETTERS:
+            oled_write_ln_P(PSTR("Letters"), false);
+            break;
+        case LAYER_NUMBERS:
+            oled_write_ln_P(PSTR("Numbers"), false);
+            break;
+        case LAYER_SYMBOLS:
+            oled_write_ln_P(PSTR("Symbols"), false);
+            break;
+        case LAYER_CONTROLS:
+            oled_write_ln_P(PSTR("Controls"), false);
+            break;
+        default:
+            oled_write_ln_P(PSTR("Undefined"), false);
+    }
+}
+
+void render_bootmagic_status(bool status) {
+    static const char PROGMEM logo[][2][3] = {
+        {{0x97, 0x98, 0}, {0xb7, 0xb8, 0}},
+        {{0x95, 0x96, 0}, {0xb5, 0xb6, 0}},
+    };
+    if (status) {
+        oled_write_ln_P(logo[0][0], false);
+        oled_write_ln_P(logo[0][1], false);
+    } else {
+        oled_write_ln_P(logo[1][0], false);
+        oled_write_ln_P(logo[1][1], false);
+    }
+}
+
+void oled_render_logo(void) {
+    static const char PROGMEM crkbd_logo[] = {
+        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f, 0x90, 0x91, 0x92, 0x93, 0x94,
+        0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5, 0xa6, 0xa7, 0xa8, 0xa9, 0xaa, 0xab, 0xac, 0xad, 0xae, 0xaf, 0xb0, 0xb1, 0xb2, 0xb3, 0xb4,
+        0xc0, 0xc1, 0xc2, 0xc3, 0xc4, 0xc5, 0xc6, 0xc7, 0xc8, 0xc9, 0xca, 0xcb, 0xcc, 0xcd, 0xce, 0xcf, 0xd0, 0xd1, 0xd2, 0xd3, 0xd4,
+        0};
+    oled_write_P(crkbd_logo, false);
+}
+
+bool oled_task_user(void) {
+    if (is_keyboard_master()) {
+        oled_render_layer_state();
+    } else {
+        oled_render_logo();
+    }
+    return false;
+}
